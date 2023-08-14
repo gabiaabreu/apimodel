b53fa362cd50dd70f7683043a3ed13a4bbf64e1e
Gabriela Abreu <gabiabr3u@gmail.com>
2023-08-14 15:03:28 -0300

    chore: fixes connection string and adds services and repository to program

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.DAL;
+using RestaurantAPI.DAL.Repositories;
 using RestaurantAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +12,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddTransient<OrdersService>();
+builder.Services.AddScoped<ClientsRepository>();
 
-string connectionString = "Server=.\\SQLExpress;Database=Restaurant;Trusted_Connection=True;TrustServerCertificate=True;";
+builder.Services.AddTransient<OrderService>();
+builder.Services.AddTransient<ClientService>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
