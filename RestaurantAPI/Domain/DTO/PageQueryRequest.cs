1544b5d9a698cdeefe1a53023b6c1e807a6f9dff
Gabriela Abreu <gabiabr3u@gmail.com>
2023-08-17 19:09:25 -0300

    feat: paged request and response

---
+++ RestaurantAPI/Domain/DTO/PageQueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Domain.DTO
+{
+    public class PageQueryRequest
+    {
+        [Range(1, int.MaxValue)]
+        public int CurrentPage { get; set; } = 1;
+
+        [Range(1, 50)]
+        public int Quantity { get; set; } = 10;
+    }
+}
---
+++ RestaurantAPI/Services/Base/ServicePagedResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace RestaurantAPI.Services.Base
+{
+    public class ServicePagedResponse<T> : ServiceResponse<IEnumerable<T>>
+    {
+        public ServicePagedResponse(IEnumerable<T> list, int count, int currentPage, int pageQuantity)
+            : base(list)
+        {
+            CurrentPage = currentPage;
+            TotalPages = (int)Math.Ceiling(count / (double)pageQuantity);
+        }
+
+        public ServicePagedResponse(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+    }
+}
